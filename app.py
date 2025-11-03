04b1f5f237566ca7e2dbd9f745b5fdd2e6a31781
Kaio <163474614+Kaio-333@users.noreply.github.com>
2025-11-03 21:59:05 +0000

    added app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+APP = Flask(__name__)
+
+@APP.get("/")
+def index():
+    return "<h1>Ola, Mundo!</h1>"
+
+if __name__ == '__main__':
+    APP.run(host="0.0.0.0", port="80") 
